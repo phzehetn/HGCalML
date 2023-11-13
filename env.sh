57978d1c6c78fcd827c2ffd95f1522e486c9bcf6
Jan Kieseler <jkiesele@cern.ch>
2023-11-13 14:51:41 +0100

    test

--- env.sh
+++ env.sh
@@ -3,6 +3,7 @@
 export HGCALML=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
 export DEEPJETCORE_SUBPACKAGE=$HGCALML
 
+
 export PATH=$HGCALML/scripts:$PATH
 export PYTHONPATH=$HGCALML/modules:$PYTHONPATH
 export LD_LIBRARY_PATH=$HGCALML/modules:$LD_LIBRARY_PATH
